3fe09573d1550dcb7347840e91811b7de998aa0b
joe <joe@example.org>
2025-10-19 17:10:14 +0200

    | 102 current  2025-10-19 17:10:08  25.05.20250910.8cd5ce8     6.16.6                          *

--- homeManagerModules/fonts.nix
+++ homeManagerModules/fonts.nix
@@ -10,8 +10,7 @@
     fonts.fontconfig.enable = true;
 
     home.packages = with pkgs; [
-      nerd-fonts.fira-code
-      nerd-fonts.droid-sans-mono
+      nerd-fonts.adwaita-mono
     ];
   };
 }
