702d1f5ec368483e12cb19f02c4d2d9d9d71e9b6
joe <joe@example.org>
2025-10-19 17:16:20 +0200

    | 106 current  2025-10-19 17:16:14  25.05.20250910.8cd5ce8     6.16.6                          *

--- nixosModules/terminal.nix
+++ nixosModules/terminal.nix
@@ -77,7 +77,7 @@
           {
             foreground = "p:green";
             properties = {
-              branch_icon = "\ue725 ";
+              branch_icon = "\uE725 ";
               cherry_pick_icon = "\e29b ";
               commit_icon = "\f417 ";
               fetch_status = false;
