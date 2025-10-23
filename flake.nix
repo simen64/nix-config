3753b13ff58f6766adbbe0e0e9ed204622324c16
simen64 <simenmunch@gmail.com>
2025-10-23 18:16:22 +0200

    | 117 current  2025-10-23 18:16:05  25.05.20251021.481cf55     6.17.4                          *

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
