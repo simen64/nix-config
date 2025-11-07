95b71f9835af75ea13f3c64d7d582a1ae68fa01e
simen64 <simenmunch@gmail.com>
2025-11-07 17:10:04 +0100

    |

--- homeManagerModules/gnome-wallpaper.nix
+++ homeManagerModules/gnome-wallpaper.nix
@@ -11,6 +11,9 @@
       "org/gnome/desktop/background" = {
         "picture-uri" = "/etc/nixos/wallpapers/View_of_Vent_in_the_Ventertal.jpg";
       };
+      "org/gnome/desktop/background" = {
+        "picture-uri-dark" = "/etc/nixos/wallpapers/View_of_Vent_in_the_Ventertal.jpg";
+      };
       "org/gnome/desktop/screensaver" = {
         "picture-uri" = "/etc/nixos/wallpapers/View_of_Vent_in_the_Ventertal.jpg";
       };
