9383a4504fba8ef9efa1bc0054cdeff8e2e2992a
simen64 <simenmunch@gmail.com>
2025-11-22 13:54:37 +0100

    fix: place imports at top

--- homeManagerModules/gnome.nix
+++ homeManagerModules/gnome.nix
@@ -1,15 +1,17 @@
 { pkgs, lib, config, ... }: {
-
+  imports = [
+    ./gnome-wallpaper.nix
+    ./gnome-standard-settings.nix
+  ];
+  
   options = {
     gnome = {
       enable = lib.mkEnableOption "Enable GNOME configuration";
     };
   };
-
+  
   config = lib.mkIf config.gnome.enable {
-    imports = [
-      ./gnome-wallpaper.nix
-      ./gnome-standard-settings.nix
-    ];
+    gnome-wallpaper.enable = lib.mkDefault true;
+    gnome-standard-settings.enable = lib.mkDefault true;
   };
 }
