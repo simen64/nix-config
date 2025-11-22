603b85005853c604b4f4ac7d53af13c67e4837fe
simen64 <simenmunch@gmail.com>
2025-11-22 13:53:40 +0100

    fix: structure better and add alejandra

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,27 +1,27 @@
 {pkgs, lib, ...}: {
 
   imports = [
-    ./dev-environment.nix
+    ./terminal.nix
+    ./podman.nix
     ./fonts.nix
     ./git.nix
-    #./gnome-standard-settings.nix
-    #./gnome-wallpaper.nix
     ./gnome.nix
     ./lazyvim.nix
-    #./rebinds.nix
+    ./rebinds.nix
     ./ssh.nix
-    ./podman.nix
   ];
 
   # Enable all modules by default
+  terminal.enable = lib.mkDefault true;
   podman.enable = lib.mkDefault false;
   fonts.enable = lib.mkDefault true;
   git.enable = lib.mkDefault true;
-  #gnome-wallpaper.enable = lib.mkDefault true;
-  #gnome-standard-settings.enable = lib.mkDefault true;
   gnome.enable = lib.mkDefault true;
   lazyvim.enable = lib.mkDefault true;
-  #rebinds.enable = lib.mkDefault true;
-  #dev-environment.enable = lib.mkDefault true;
+  rebinds.enable = lib.mkDefault true;
   ssh.enable = lib.mkDefault true;
+
+  home.packages = with pkgs; [
+    alejandra
+  ];
 }
