34774f5fadaa1d717d01f4de37c0400bb117a0a9
simen64 <simenmunch@gmail.com>
2025-12-01 20:42:03 +0100

    chore: remove nix.settings from thinkpad configuration.nix |   62   2025-12-01 20:41:58   (current)

--- darwin/default.nix
+++ darwin/default.nix
@@ -12,19 +12,6 @@
     inputs.nix-homebrew.darwinModules.nix-homebrew
   ];
 
-  # nix config
-  #nix = {
-  #  settings = {
-  #    experimental-features = [
-  #      "nix-command"
-  #      "flakes"
-  #    ];
-  #    # disabled due to https://github.com/NixOS/nix/issues/7273
-  #    # auto-optimise-store = true;
-  #  };
-  #  enable = false; # using determinate installer
-  #};
-
   nixpkgs.config.allowUnfree = true;
 
   nix-homebrew = {
