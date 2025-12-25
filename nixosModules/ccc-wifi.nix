fc7cb94fa10991ec174e32570f3dc6dc5faaa227
simen64 <simenmunch@gmail.com>
2025-12-25 11:33:36 +0100

    feat: add congress wifi module

---
+++ nixosModules/ccc-wifi.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.etc."NetworkManager/system-connections/39C3.nmconnection".text = ''
+    [connection]
+    id=39C3
+    uuid=c80101e2-7b99-4511-846b-2388eb86a5ad
+    type=wifi
+
+    [wifi]
+    mode=infrastructure
+    ssid=39C3
+
+    [wifi-security]
+    auth-alg=open
+    key-mgmt=wpa-eap
+
+    [802-1x]
+    altsubject-matches=DNS:radius.c3noc.net
+    ca-cert=/etc/ssl/certs/ISRG_Root_X1.pem
+    eap=ttls
+    identity=outboundonly
+    password=outboundonly
+    phase2-auth=pap
+
+    [ipv4]
+    method=auto
+
+    [ipv6]
+    method=auto
+  '';
+}
