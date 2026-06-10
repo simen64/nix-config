09e0f5d59db5cfc1d53d3dbcd374dee0468f0a4c
simen64 <simenmunch@gmail.com>
2026-06-10 19:19:42 +0200

    feat: update dms

--- homeManagerModules/git.nix
+++ homeManagerModules/git.nix
@@ -18,19 +18,29 @@
         user.name = "simen64";
       };
 
-      signing.key = "~/.ssh/id_ed25519_sk_rk-USB-C.pub";
+      signing.key = "~/.ssh/id_ed25519_sk_rk";
       signing.signByDefault = true;
       signing.format = "ssh";
 
-      includes = lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-        {
-          condition = "gitdir:~/nix/";
-          path = pkgs.writeText "nix-repo-gitconfig" ''
-            [user]
-              signingKey = /Users/simen/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/PublicKeys/fedda82f4d015e82453dd083b4a90cc3.pub
-          '';
-        }
-      ];
+      includes =
+        (lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+          {
+            condition = "gitdir:~/nix/";
+            path = pkgs.writeText "nix-repo-gitconfig" ''
+              [user]
+                signingKey = /Users/simen/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/PublicKeys/fedda82f4d015e82453dd083b4a90cc3.pub
+            '';
+          }
+        ])
+        ++ (lib.optionals pkgs.stdenv.isLinux [
+          {
+            condition = "gitdir:/etc/nixos/";
+            path = pkgs.writeText "nixos-repo-gitconfig" ''
+              [user]
+                signingKey = ~/.ssh/tpm_key.tpm
+            '';
+          }
+        ]);
     };
 
     services = {
--- nixosModules/niri.nix
+++ nixosModules/niri.nix
@@ -36,5 +36,6 @@
     services.gvfs.enable = true;
     services.devmon.enable = true;
     services.udisks2.enable = true;
+    services.upower.enable = true;
   };
 }
