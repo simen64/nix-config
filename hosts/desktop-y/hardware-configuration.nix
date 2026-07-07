3151480a88273e9381d93b4f7e74e5df51073c69
simen64 <simenmunch@gmail.com>
2026-07-07 14:48:18 +0200

    desktop-y: 11

--- hosts/desktop-y/hardware-configuration.nix
+++ hosts/desktop-y/hardware-configuration.nix
@@ -36,4 +36,8 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true;
 }
