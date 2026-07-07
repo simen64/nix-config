47080fdb65244260b790ca0ab27ec9bd6fc49aaf
simen64 <simenmunch@gmail.com>
2026-07-07 15:05:53 +0200

    desktop-y: 12

--- nixosModules/niri.nix
+++ nixosModules/niri.nix
@@ -42,5 +42,17 @@
     services.devmon.enable = true;
     services.udisks2.enable = true;
     services.upower.enable = true;
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
+      ];
+      config = {
+        common.default = ["gnome"];
+      };
+    };
   };
 }
